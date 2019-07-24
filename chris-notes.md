6d2ab18b43e9c11869c243a028787a62420838be
Matt McKenzie <matthew.mitchell.mckenzie@gmail.com>
2019-07-24 16:48:18 +1000

    first commit

---
+++ chris-notes.md
@@ -0,0 +1,16 @@
+## Notes for Chris
+
+- Terminal
+- VSCode
+- Homebrew
+- Rbenv
+- pry
+- Ruby
+    - integer
+    - float
+    - string
+    - modulo
+    - nil
+    - booleans (true and false)
+    - conditionals
+    - puts
---
+++ chris_prac.rb
@@ -0,0 +1,26 @@
+my_variable = 2 * 6
+puts(my_variable)
+
+my_variable = 3.0 * 7 
+
+if(my_variable < 15) 
+    puts(my_variable)
+    my_variable = "HELLO there this is a string"
+    puts(my_variable)
+    other_var = 23 / 7
+    puts(other_var)
+    other_var = 29 % 6
+    puts(other_var)
+else
+    # puts(this_is_a_useful_name)
+    # this_is_a_useful_name
+    numer_of_species_in_your_zoo = 6
+    # numer_of_species_in_your_zoo
+    str2 = "Some other string"
+    puts(str2)
+end
+
+puts("AFTER CONDITIONAL")
+puts(my_variable)
+
+puts("End of the file")
